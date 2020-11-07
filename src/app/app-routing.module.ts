3806461b019363ce295d6421bdda5405414be1d7
mauriciorepo <mauriciovalencas@gmail.com>
2020-11-07 11:46:39 -0300

    Test unit jest configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  //Quando
-  //{path:'companies', loadChildren:'src/app/pages/companies/companies.module#CompaniesModule'}
+  
+  
   {path:'companies', loadChildren:()=>import('src/app/pages/companies/companies.module').then(m=>m.CompaniesModule)}
 ];
 
